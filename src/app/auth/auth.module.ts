11a62e550a43bd98fdc579d37989e2630554e161
Finn <finn@webmasterfreelanceonlinedev.site>
2020-02-17 15:18:25 +0400

    Added angular material, core & shared modules, mat-stepper sample

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './register/register.component';
+import {SharedModule} from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [RegisterComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
