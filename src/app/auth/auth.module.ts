70258573407ac193607428b55d5d71207f8720a2
Finn <=finn@webmasterfreelanceonlinedev.site>
2020-02-17 16:16:53 +0400

    create new-company page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './register/register.component';
+import { NewCompanyComponent } from './new-company/new-company.component';
 
 
 @NgModule({
-  declarations: [RegisterComponent],
+  declarations: [RegisterComponent, NewCompanyComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
